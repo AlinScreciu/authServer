8940272ec3094a5764bd5944defff28b9c515660
Alin <alin.screciu01@e-uvt.ro>
2022-01-04 00:25:51 +0200

    da fuck

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,8 @@ import dotenv from 'dotenv'
 dotenv.config();
 var codes = [];
 const updatePass = (req, res, next) => {
+    for (item in codes)
+        console.log(`${JSON.stringify(item,0,2)}`);
     const targetUser = codes.filter(obj => {return obj === req.body.email});
     if (!targetUser.length) return res.status(409).json({ 
         message: "No verification code was sent for this email", 
