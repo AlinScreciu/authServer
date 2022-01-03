7ea7f5acca38ad85b57ebc0bd5bf1059529278d6
Alin <alin.screciu01@e-uvt.ro>
2022-01-03 23:52:37 +0200

    check for email first

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,8 +9,7 @@ dotenv.config();
 var codes = [];
 const updatePass = (req, res, next) => {
     const targetUser = codes.filter(obj => {return obj === req.body.email});
-    console.log(targetUser.length, !targetUser);
-    if (!targetUser) return res.status(409).json({ 
+    if (!targetUser.length) return res.status(409).json({ 
         message: "No verification code was sent for this email", 
         success: false
     });
